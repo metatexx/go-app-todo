adaf743cc17636106b9053d541484e6e6d17731e
Niklas Kopyciok <n.kopyciok@metatexx.de>
2022-07-28 08:21:22 +0200

    Cleanup

--- frontend/todos.go
+++ frontend/todos.go
@@ -37,10 +37,13 @@ func (c *ToDoList) Render() app.UI {
 		// this gets called on the server before the page is delivered
 		return app.Div().Text("app is loading")
 	}
-	return app.Section().Class("todoapp").Body(
+	return app.Section().
+		Class("todoapp").Body(
 		func() app.UI {
 			if c.updateAvailable {
-				return app.H1().Style("text-align", "center").Text("Update available, please reload.")
+				return app.H1().
+					Style("text-align", "center").
+					Text("Update available, please reload.")
 			}
 			return app.Div()
 		}(),
@@ -68,70 +71,47 @@ func (c *ToDoList) Render() app.UI {
 		app.Div().Body(
 			app.Ul().Body(
 				app.Range(c.todos).Slice(func(i int) app.UI {
-					if (c.filterMode == 1 && c.todos[i].Done) || (c.filterMode == 2 && !c.todos[i].Done) {
+					if (c.filterMode == 1 && c.todos[i].Done) ||
+						(c.filterMode == 2 && !c.todos[i].Done) {
 						return app.Span()
 					}
 					return app.Li().
-						Styles(map[string]string{
-							"text-decoration": func() string {
-								if c.todos[i].Done {
-									return "line-through"
-								}
-								return "none"
-							}(),
-						}).Body(
-						app.Label().Body(
-							app.Input().
-								Type("checkbox").
-								Checked(c.todos[i].Done).
-								OnChange(c.toggleDone(c.todos[i]), c.todos[i]),
-							app.Text(c.todos[i].Text),
-						),
-						app.Div().
-							Class("destroy").
-							Text("✕").
-							OnClick(c.deleteHandler(c.todos[i]), c.todos[i]))
+						Body(
+							app.Label().Body(
+								app.Input().
+									Type("checkbox").
+									Checked(c.todos[i].Done).
+									OnChange(func(ctx app.Context, e app.Event) {
+										c.todos[i].Done = !c.todos[i].Done
+									}),
+								app.Span().Styles(map[string]string{
+									"text-decoration": func() string {
+										if c.todos[i].Done {
+											return "line-through"
+										}
+										return "none"
+									}(),
+								}).Body(
+									app.Text(c.todos[i].Text),
+								),
+							),
+							app.Div().
+								Class("delete").
+								Text("✕").
+								OnClick(func(ctx app.Context, e app.Event) {
+									c.todos = append(c.todos[:i], c.todos[i+1:]...)
+								}))
 				}),
 				app.Li().Body(
 					app.Div().
 						Class("grid").Body(
 						app.Div().
-							Text(c.generateLeftItemsOutput()),
+							Text(fmt.Sprintf("%d items left", c.countUncompleted())),
 						app.Div().
 							Class("selection").Body(
-							func() app.UI {
-								if c.filterMode == 0 {
-									return app.Button().
-										Text("All").
-										OnClick(c.switchSelection(0)).
-										Class("active")
-								}
-								return app.Button().
-									Text("All").
-									OnClick(c.switchSelection(0))
-							}(),
-							func() app.UI {
-								if c.filterMode == 1 {
-									return app.Button().
-										Text("Active").
-										OnClick(c.switchSelection(1)).
-										Class("active")
-								}
-								return app.Button().
-									Text("Active").
-									OnClick(c.switchSelection(1))
-							}(),
-							func() app.UI {
-								if c.filterMode == 2 {
-									return app.Button().
-										Text("Completed").
-										OnClick(c.switchSelection(2)).
-										Class("active")
-								}
-								return app.Button().
-									Text("Completed").
-									OnClick(c.switchSelection(2))
-							}(),
+							c.generateFilterButton("All", 0, c.filterMode == 0),
+							c.generateFilterButton("Active", 1, c.filterMode == 1),
+							c.generateFilterButton("Completed", 2, c.filterMode == 2),
 						),
 						func() app.UI {
 							if !c.hasCompletedTodo() {
@@ -152,52 +132,28 @@ func (c *ToDoList) Render() app.UI {
 	)
 }
 
-func (c *ToDoList) deleteHandler(item *todo) func(ctx app.Context, e app.Event) {
-	return func(ctx app.Context, e app.Event) {
-		for i, v := range c.todos {
-			if v == item {
-				c.todos = append(c.todos[:i], c.todos[i+1:]...)
-				return
-			}
-		}
-	}
-}
-
-func (c *ToDoList) toggleDone(item *todo) func(ctx app.Context, e app.Event) {
-	return func(ctx app.Context, e app.Event) {
-		item.Done = !item.Done
-	}
-}
-
-func (c *ToDoList) onSubmit(ctx app.Context, e app.Event) {
+func (c *ToDoList) onSubmit(_ app.Context, e app.Event) {
 	e.PreventDefault()
-
+	if c.inputTodo == "" {
+		return
+	}
 	c.todos = append(c.todos, &todo{Text: c.inputTodo})
 	c.inputTodo = ""
 }
 
-func (c *ToDoList) generateLeftItemsOutput() string {
+func (c *ToDoList) countUncompleted() int {
 	sum := 0
 	for _, v := range c.todos {
 		if !v.Done {
 			sum++
 		}
 	}
-	return fmt.Sprintf("%v items left", sum)
+	return sum
 }
 
 func (c *ToDoList) hasCompletedTodo() bool {
 	for _, v := range c.todos {
 		if v.Done {
-			return true
-		}
-	}
-	return false
-}
-
-func (c *ToDoList) hasUncompletedTodo() bool {
-	for _, v := range c.todos {
-		if !v.Done {
 			return true
 		}
 	}
@@ -221,10 +177,10 @@ func (c *ToDoList) switchSelection(mode int) func(ctx app.Context, e app.Event) 
 	}
 }
 
-func (c *ToDoList) toggleAllDone(ctx app.Context, e app.Event) {
+func (c *ToDoList) toggleAllDone(_ app.Context, e app.Event) {
 	e.PreventDefault()
 	newDone := false
-	if c.hasUncompletedTodo() {
+	if !c.hasCompletedTodo() {
 		newDone = true
 	}
 
@@ -232,4 +188,16 @@ func (c *ToDoList) toggleAllDone(ctx app.Context, e app.Event) {
 		v.Done = newDone
 	}
 
+}
+
+func (c *ToDoList) generateFilterButton(text string, mode int, active bool) app.UI {
+	if active {
+		return app.Button().
+			Text(text).
+			OnClick(c.switchSelection(mode)).
+			Class("active")
+	}
+	return app.Button().
+		Text(text).
+		OnClick(c.switchSelection(mode))
 }
